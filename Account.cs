35af28b948eaaf2e3277c2d948d7f4e035251fc8
pajjukn <83894608+pajjukn@users.noreply.github.com>
2022-12-09 17:13:48 +0530

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOfSuccess.EntityLayer
+{
+    //Entity Class-that defines the structure of Account class
+    public class Account
+    {
+        
+        static int count = 999;
+        public int AccountNumber { get; set; }
+        public string Name { get; set; }
+        public int PinNumber { get; set; }
+        public double Balance { get; set; }
+        public Privilge Privilge { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ActivatedDate { get; set; }
+        public DateTime ClosedDate { get; set; }
+
+        public Account()
+        {
+            count++;
+            this.AccountNumber = count;
+        }
+    }
+}
